6c433508c94d4779d85554641334f322d570ad89
kich555 <hyunjjang199582@gmail.com>
2022-10-29 18:40:58 +0900

    Install & setting axios

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,3 +1,12 @@
-export default function generateJoke() {
-  return "This is a joke";
+import axios from 'axios';
+
+export default async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    }
+  }
+
+  const res = await axios.get('https://icanhazdadjoke.com/', config) 
+  document.getElementById('joke').innerHTML = res.data.joke; // display the joke in the DOM
 }
--- src/index.js
+++ src/index.js
@@ -5,4 +5,7 @@ import laughing from './assets/laughing.svg';
 const laughImg = document.getElementById('laughImg')
 laughImg.src = laughing
 
-console.log(generateJoke());
+const jokeBtn = document.getElementById('jokeBtn') // get the joke button from the DOM
+jokeBtn.addEventListener('click', generateJoke) // add event listener to the joke button
+
+generateJoke(); // generate a joke on page load
