02b5c579271751708077b44a30a3703adf754cb6
kich555 <hyunjjang199582@gmail.com>
2022-10-29 17:52:08 +0900

    Setting webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,17 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
+        filename: '[name][contenthash].js'
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000, // default is 8080
+        open: true, // open browser on start
+        hot: true, // hot module replacement
+        compress: true, // enable gzip compression
+        historyApiFallback: true, // fallback to index.html for SPA routing
     },
     module: {
         rules: [
